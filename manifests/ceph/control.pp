21f0fffb1d6085a27a8babe423b1737fc95a4ff6
dotalton <dotalton@cisco.com>
2013-08-03 15:20:06 -0700

    permission changes and fixed variable scope

--- manifests/ceph/control.pp
+++ manifests/ceph/control.pp
@@ -1,4 +1,6 @@
-class coe::ceph::control() {
+class coe::ceph::control(
+  $fsid = $::ceph_monitor_fsid,
+) {
 
   include 'ceph::package'
 
@@ -11,11 +13,6 @@ class coe::ceph::control() {
     require => Apt::Source['ceph'],
   }
 
-  package { 'python-ceph':
-    ensure => present,
-    require => Apt::Source['ceph'],
-  }
- 
   class { 'ceph::conf':
     fsid      => $fsid,
     conf_owner => 'glance',
@@ -24,27 +21,27 @@ class coe::ceph::control() {
 
   file { '/etc/ceph/client.admin':
     ensure => present,
-    owner => 'cinder',
-    group => 'cinder',
+    owner => 'glance',
+    group => 'glance',
     mode  => '660',
     require => Exec['copy the admin key to make glance work'],
   }
 
   file { '/etc/ceph/keyring':
-    owner => 'cinder',
-    group => 'cinder',
+    owner => 'glance',
+    group => 'glance',
     mode  => 0600,
   }
-  
-  exec { 'copy the admin key to make cinder work':
+
+  exec { 'copy the admin key to make glance work':
     command => 'cp /etc/ceph/keyring /etc/ceph/client.admin',
     creates => '/etc/ceph/client.admin',
     require => [ Package['ceph'], Ceph::Key['admin'] ],
   }
 
   exec { 'create the pool':
-    command => "/usr/bin/ceph osd pool create ${glance_store_pool} 128",
-    unless  => "/usr/bin/rados lspools | grep -sq ${glance_store_pool}",
+    command => "/usr/bin/ceph osd pool create ${::glance_ceph_pool} 128",
+    unless  => "/usr/bin/rados lspools | grep -sq ${::glance_ceph_pool}",
     require => Exec['copy the admin key to make glance work'],
   }
 
