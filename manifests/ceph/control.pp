6871cf2073c31948659012a4553756740cc243b6
Chris Ricker <chricker@cisco.com>
2013-08-26 08:59:09 -0700

    Bug 1215087 create pool for glance on controller
    Conditionally create glance pool if
    - using glance on ceph
    - pool doesn't already exist
    - node is controller

--- manifests/ceph/control.pp
+++ manifests/ceph/control.pp
@@ -16,25 +16,27 @@ class coe::ceph::control(
   file { '/etc/ceph/client.admin':
     ensure  => present,
     mode    => 0644,
-    require => Exec['copy the admin key to make glance work'],
+    require => Exec['copy the admin key'],
   }
 
   file { '/etc/ceph/keyring':
     mode    => 0644,
-    require => Exec['copy the admin key to make glance work'],
+    require => Exec['copy the admin key'],
   }
 
-  exec { 'copy the admin key to make glance work':
+  exec { 'copy the admin key':
     command => 'cp /etc/ceph/keyring /etc/ceph/client.admin',
     creates => '/etc/ceph/client.admin',
     require => Package['ceph'],
   }
 
-  exec { 'create the pool':
-    command => "/usr/bin/ceph osd pool create ${::glance_ceph_pool} 128",
-    unless  => "/usr/bin/rados lspools | grep -sq ${::glance_ceph_pool}",
-    require => Exec['copy the admin key to make glance work'],
-    notify  => [ Service['glance-api'], Service['glance-registry'] ],
+  if $::glance_ceph_enabled {
+    exec { 'create the pool':
+      command => "/usr/bin/ceph osd pool create ${::glance_ceph_pool} 128",
+      unless  => "/usr/bin/rados lspools | grep -sq ${::glance_ceph_pool}",
+      require => Exec['copy the admin key'],
+      notify  => [ Service['glance-api'], Service['glance-registry'] ],
+    }
   }
 
 }
